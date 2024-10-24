6b3ded3eb82cd4562f503946324b7699dfadfca3
36_BE_IT_B Musaddiq Shaikh <musaddiqshaikh42001@gmail.com>
2024-10-24 23:36:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CounterApp from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterApp />
+    </div>
+  );
+}
+
+export default App;
